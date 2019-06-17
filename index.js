9f57894e55bdd22c6f43fdd1212f6e7ac3e5c48d
ANonymous <postmaster.richmama@gmail.com>
2019-06-17 11:33:17 +0100

    added express and bootstrap

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
-var http = require('http');
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+const secret = 'jgfddhdDQ3$ADQFXS';
 
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: secret, saveUninitialized: true, resave: true }));
+//app.set('views',"/storage/emulated/0/node/voting/views");
+app.set('views',"/voting/views");
+app.set('view engine', 'ejs');
 
-const port = process.env.PORT || 3000;
-const server = http.createServer(function(req, res) {
-	
- /*hello('Testing Edge in NodeJS',function(error,result){
-	if(error) throw error;
-	console.log(result);
-});*/
-	
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<h3 style=\'color:red;\'>NodeJS is here</blockquote></center>');
-  res.end('');
-
- 
+app.get('/', function (req, res) {
+  res.render('index', {name: null, vote: null, error: null});
 });
-server.listen(port, function() {
-  console.log('Server running at port: ' + port);
+
+app.post('/', function(req,res){
+	let name = req.body.name;
+	let age = req.body.age;
+	let vote = req.body.vote;
+	let uu = "http://localhost/live/vote.php";
+	let sess = req.session;
+	
+	if(name == "" || age == "" || vote == ""){
+		sess.name = name; sess.age = age; sess.vote = vote; 
+		res.render('index',{error: "Please fill in the required fields"});
+	}
+	
+	else{
+		request(uu,function(err,response,body){
+		   if(err){
+			res.render('index',{name: null, vote: null, error: "An error occurred while processing your request"});
+		   }
+		
+		   else{
+			
+		   }
+		});
+	}
+});
+
+app.listen(3000, function () {
+  console.log('NodeJS app listening on port 3000!')
 });
