66f39c33fde7215a43783b4fbf3d024a90d4ff77
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-06-23 14:39:42 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => {
-  let data = '{"em": "kudayisitobi@gmail.com","title": "Nodemailer says HI","content":"<h3>NodeMailer says HI</h3><p>Welcome to MailNinja, our first bulk SMTP mailer built with NodeJS and of course Laravel 5 :)</p>"}';
+  let data = '{
+  	            "receivers": "kkudayisitobi@gmail.com, kudayisitobi@gmail.com",
+                  "subject": "Nodemailer says HI ?",
+                  "text_body": "NodeMailer says HI\nWelcome to MailNinja, our first bulk SMTP mailer built with NodeJS and of course Laravel 5 :)",
+                  "html_body": "<h3>NodeMailer says HI</h3><p>Welcome to MailNinja, our first bulk SMTP mailer built with NodeJS and of course Laravel 5 :)</p>"
+                }';
+  
   result = sendMail(data);
    res.render('index',{result: result});  
   })
@@ -21,9 +27,38 @@ express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
   
   
-  function sendMail(data)
+  async function sendMail(data)
   {  	
       let ret = JSON.parse(data);
       console.log(ret);
       return ret;
+      
+        // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  //let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: config.smtp.host,
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: config.smtp.user, // generated ethereal user
+      pass: config.smtp.pass // generated ethereal password
+    }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Mail Ninja ??" <support@mailninja.com>', // sender address
+    to: data.receivers, // list of receivers
+    subject: data.subject, // Subject line
+    text: data.text_body, // plain text body
+    html: data.html_body // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  return {"status": "ok"};
   }
