f463e0b3497766feedc95280ae19c80173c25ed4
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-06-23 16:16:57 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -60,5 +60,5 @@ express()
   console.log("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  return {"status": "ok"};
+  resolve({"status": "ok"});
   }
