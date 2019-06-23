d715442b8809d0cc47e4b89ffc3b98abf6c70d14
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-06-23 14:02:57 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => {
-  let data = {'em':'kudayisitobi@gmail.com','title':'Nodemailer says HI','content':'<h3>NodeMailer says HI</h3><p>Welcome to MailNinja, our first bulk SMTP mailer built with NodeJS and of course Laravel 5 :)</p>'};
+  let data = '{"em": "kudayisitobi@gmail.com","title": "Nodemailer says HI","content":"<h3>NodeMailer says HI</h3><p>Welcome to MailNinja, our first bulk SMTP mailer built with NodeJS and of course Laravel 5 :)</p>"}';
   result = sendMail(data);
    res.render('index',{result: result});  
   })
@@ -25,5 +25,5 @@ express()
   {  	
       let ret = JSON.parse(data);
       console.log(ret);
-      return data;
+      return ret;
   }
